10c40391cd841bfcf657cf33fa6224b5f9f21153
zodywoolsey <zodie@pupper.dev>
2025-11-02 10:37:56 -0700

    began work on ffmpeg decode function, still learning a bit so consider this the discovery testing

--- src/gdffmpeg.cpp
+++ src/gdffmpeg.cpp
@@ -2,24 +2,161 @@
 
 using namespace godot;
 
-int FFmpegTexture::test(){
-	codec = avcodec_find_decoder(AVCodecID::AV_CODEC_ID_MPEG4);
-	return 0;
-}
-
 void FFmpegTexture::_bind_methods() {
 	godot::ClassDB::bind_method(D_METHOD("exists"), &FFmpegTexture::exists);
 }
 
 bool FFmpegTexture::exists() {
-	if(test() == 0){
-		return true;
-	}
-	else {
-		return false;
-	}
+	return true;
+}
+
+bool decode_from_bytes(PackedByteArray bytes) {
+	return true;
 }
 
 void FFmpeg::_bind_methods() {
 	return;
+}
+
+static void _decode(AVCodecContext *dec_ctx, AVFrame *frame, AVPacket *pkt, const char *filename)
+{
+    char buf[1024];
+    int av_send_packet;
+
+    av_send_packet = avcodec_send_packet(dec_ctx, pkt);
+    if (av_send_packet < 0) {
+        fprintf(stderr, "Error sending a packet for decoding\n");
+        exit(1);
+    }
+
+    while (av_send_packet >= 0) {
+        av_send_packet = avcodec_receive_frame(dec_ctx, frame);
+        if (av_send_packet == AVERROR(EAGAIN) || av_send_packet == AVERROR_EOF)
+            return;
+        else if (av_send_packet < 0) {
+            fprintf(stderr, "Error during decoding\n");
+            exit(1);
+        }
+
+        //printf("saving frame %3"PRId64"\n", dec_ctx->frame_num);
+        fflush(stdout);
+
+        /* the picture is allocated by the decoder. no need to
+           free it */
+        //snprintf(buf, sizeof(buf), "%s-%"PRId64, filename, dec_ctx->frame_num);
+        //pgm_save(frame->data[0], frame->linesize[0],
+                 //frame->width, frame->height, buf);
+    }
+}
+
+#define INBUF_SIZE 4096
+
+int main(int argc, char **argv)
+{
+    const char *filename, *outfilename;
+    const AVCodec *codec;
+    AVCodecParserContext *parser;
+    AVCodecContext *avcodec_context3 = NULL;
+    FILE *file;
+    AVFrame *frame;
+    uint8_t inbuf[INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
+    uint8_t *data;
+    size_t   data_size;
+    int av_send_packet;
+    int eof;
+    AVPacket *pkt;
+
+    if (argc <= 2) {
+        fprintf(stderr, "Usage: %s <input file> <output file>\n"
+                "And check your input file is encoded by mpeg1video please.\n", argv[0]);
+        exit(0);
+    }
+    filename    = argv[1];
+    outfilename = argv[2];
+
+    pkt = av_packet_alloc();
+    if (!pkt)
+        exit(1);
+
+    /* set end of buffer to 0 (this ensures that no overreading happens for damaged MPEG streams) */
+    memset(inbuf + INBUF_SIZE, 0, AV_INPUT_BUFFER_PADDING_SIZE);
+
+    /* find the MPEG-1 video decoder */
+    codec = avcodec_find_decoder(AV_CODEC_ID_MPEG1VIDEO);
+    if (!codec) {
+        fprintf(stderr, "Codec not found\n");
+        exit(1);
+    }
+
+    parser = av_parser_init(codec->id);
+    if (!parser) {
+        fprintf(stderr, "parser not found\n");
+        exit(1);
+    }
+
+    avcodec_context3 = avcodec_alloc_context3(codec);
+    if (!avcodec_context3) {
+        fprintf(stderr, "Could not allocate video codec context\n");
+        exit(1);
+    }
+
+    /* For some codecs, such as msmpeg4 and mpeg4, width and height
+       MUST be initialized there because this information is not
+       available in the bitstream. */
+
+    /* open it */
+    if (avcodec_open2(avcodec_context3, codec, NULL) < 0) {
+        fprintf(stderr, "Could not open codec\n");
+        exit(1);
+    }
+
+    file = fopen(filename, "rb");
+    if (!file) {
+        fprintf(stderr, "Could not open %s\n", filename);
+        exit(1);
+    }
+
+    frame = av_frame_alloc();
+    if (!frame) {
+        fprintf(stderr, "Could not allocate video frame\n");
+        exit(1);
+    }
+
+    do {
+        /* read raw data from the input file */
+        data_size = fread(inbuf, 1, INBUF_SIZE, file);
+        if (ferror(file))
+            break;
+        eof = !data_size;
+
+        /* use the parser to split the data into frames */
+        data = inbuf;
+        while (data_size > 0 || eof) {
+            av_send_packet = av_parser_parse2(parser, avcodec_context3, &pkt->data, &pkt->size,
+                                   data, data_size, AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
+            if (av_send_packet < 0) {
+                fprintf(stderr, "Error while parsing\n");
+                exit(1);
+            }
+            data      += av_send_packet;
+            data_size -= av_send_packet;
+
+            if (pkt->size)
+                _decode(avcodec_context3, frame, pkt, outfilename);
+            else if (eof)
+                break;
+        }
+    } while (!eof);
+
+    /* flush the decoder */
+    _decode(avcodec_context3, frame, NULL, outfilename);
+
+    fclose(file);
+
+    av_parser_close(parser);
+    avcodec_free_context(&avcodec_context3);
+    av_frame_free(&frame);
+    av_packet_free(&pkt);
+
+    return 0;
 }
--- src/gdffmpeg.h
+++ src/gdffmpeg.h
@@ -1,16 +1,37 @@
 #pragma once
 
+// this is an interface to use ffmpeg within godot
+// the goal is to provide a simplified FFmpegTexture
+// where the end user needs to put in minimal effort
+// and provide wrappers for everything else to be
+// accessible from gdscript or whatever script lang
+// the user is using for their work in godot
+//
+// for contributions:
+// make sure code fits the style and is well organized
+// and reabable. it is very important that our codebases
+// remain accessible and readable for anyone who may need
+// to modify or learn from the codebase in the future,
+// even if they do not have prior knowledge of the systems
+
 #include "godot_cpp/classes/texture2d.hpp"
 #include "godot_cpp/classes/wrapped.hpp"
 #include "godot_cpp/classes/object.hpp"
 #include "godot_cpp/variant/variant.hpp"
+#include <stdio.h>
 
 extern "C" {
 #include "ffmpeg/libavcodec/codec.h"
+#include "ffmpeg/libavcodec/avcodec.h"
 }
 
 using namespace godot;
-// awwooo
+
+// this is a simplified helper utility for end users
+// to shove a thing to be decoded into and get an
+// AudioStream and a Textuere out of it, so they can
+// easily bypass the complexity of using ffmpeg
+
 class FFmpegTexture : public Texture2D {
 	GDCLASS(FFmpegTexture, Texture2D)
 
@@ -21,11 +42,12 @@ public:
 	FFmpegTexture() = default;
 	~FFmpegTexture() override = default;
 	const AVCodec *codec;
-	int test();
 	bool exists();
-//	void print_type(const Variant &p_variant) const;
+	bool decode_from_bytes(PackedByteArray bytes);
 };
 
+// the root class that provides access to constants and variables
+// relevant to using FFmpeg in godot
 
 class FFmpeg : public Object {
 	GDCLASS(FFmpeg, Object)
@@ -38,6 +60,8 @@ class FFmpeg : public Object {
 		~FFmpeg() override = default;
 };
 
+// wrapper class for accessing AVCodec functionality from within godot
+
 class GDAVCodec : public RefCounted {
 	GDCLASS(GDAVCodec, RefCounted)
 
@@ -47,4 +71,40 @@ class GDAVCodec : public RefCounted {
 	public:
 		GDAVCodec() = default;
 		~GDAVCodec() override = default;
+		static int find_decoder();
+
+		const AVCodec *codec;
+
+};
+
+// wrapper class for accessing AVCodecParserContext functionality from within godot
+
+class GDAVCodecParserContext : public RefCounted {
+	GDCLASS(GDAVCodecParserContext, RefCounted)
+
+	protected:
+		static void _bind_methods();
+
+	public:
+		//GDAVCodecParserContext() = default;
+		~GDAVCodecParserContext() override = default;
+
+		AVCodecParserContext *parser_context;
+
+};
+
+// wrapper class for accessing AVCodecParserContext functionality from within godot
+
+class GDAVFrame : public RefCounted {
+	GDCLASS(GDAVFrame, RefCounted)
+
+	protected:
+		static void _bind_methods();
+
+	public:
+		//GDAVCodecParserContext() = default;
+		~GDAVFrame() override = default;
+
+		AVFrame *avframe;
+
 };
